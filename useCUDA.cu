dbac043556bbbf6c608fd19031ee0a1f752e0c39
xiaotaw <xiaotaw@qq.com>
2020-02-20 09:38:09 +0800

    add Eigen headers in useCUDA.cu
    'The log suggests the problem are with nvcc compiler', said weigao95.

--- useCUDA.cu
+++ useCUDA.cu
@@ -1,5 +1,9 @@
 #include "useCUDA.h"
 #include <stdio.h>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+//#include <boost/scoped_ptr.hpp>
+
 __global__ void saxpy(int n, float a, float *x, float *y)
 {
   int i = blockIdx.x*blockDim.x + threadIdx.x;
